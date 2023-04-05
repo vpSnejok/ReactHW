139ef8fa006631bec4ae9ddcd91e4eb2b74c0452
Vitaliy Snezhkin <vpsnejok@gmail.com>
2023-04-06 01:03:04 +0300

    add

--- hw2/src/components/CardList/Card/Card.jsx
+++ hw2/src/components/CardList/Card/Card.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
-import "./Card.scss"
+import "./Card.scss";
 import Button from "../../Button/Button";
 import { ReactComponent as StarBlack } from "../../../assets/img/star-black.svg";
 import { ReactComponent as StarGold } from "../../../assets/img/star-gold.svg";
 
 class Card extends Component {
   render() {
-    const { price, img, id, color, name, setFavorites, isFavorite } =
-      this.props;
+    const {
+      price,
+      img,
+      id,
+      color,
+      name,
+      setFavorites,
+      isFavorite,
+      handleOpenModal,
+    } = this.props;
 
     return (
       <li>
@@ -15,10 +23,10 @@ class Card extends Component {
         <h2>{name}</h2>
         <p>Price: {price}</p>
         <p>Color: {color}</p>
-        <Button text="add to card" classes="btn btn__add-card" />
-        <span onClick={() => setFavorites({ price, img, id, color, name })}>
+        <p onClick={() => setFavorites({ price, img, id, color, name })}>
           {isFavorite ? <StarGold /> : <StarBlack />}
-        </span>
+        </p>
+        <Button text="Додати у кошик" classes="btn btn__add-card" />
       </li>
     );
   }
